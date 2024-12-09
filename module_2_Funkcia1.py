f373d9c20dea9b4c4e29f475995b3422e74d322c
edward-ga11 <gae05120@gmail.com>
2024-12-09 08:38:51 +0300

    Add files via upload

---
+++ module_2_Funkcia1.py
@@ -0,0 +1,19 @@
+def get_matrix(n, m, value):
+    if n <= 0 or m <= 0:
+        return []
+
+    matrix = []
+
+    for i in range(n):
+        row = [value] * m
+        matrix.append(row)
+
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+
+print(result1)
+print(result2)
+print(result3)
